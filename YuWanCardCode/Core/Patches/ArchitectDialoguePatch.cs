14750bf8ff471aafe97c1597a539c2482ea6b1d8
YuWan_SAMA <145727116+YuWan886@users.noreply.github.com>
2026-05-01 14:19:40 +0800

    refactor: 修复建筑师对话卡住的问题

--- YuWanCardCode/Core/Patches/ArchitectDialoguePatch.cs
+++ YuWanCardCode/Core/Patches/ArchitectDialoguePatch.cs
@@ -1,28 +1,33 @@
 using HarmonyLib;
 using MegaCrit.Sts2.Core.Entities.Ancients;
-using MegaCrit.Sts2.Core.Models.Events;
-using YuWanCard.Core.Utils;
+using MegaCrit.Sts2.Core.Models;
 
 namespace YuWanCard.Core.Patches;
 
 [HarmonyPatch]
 public static class ArchitectDialoguePatch
 {
-    static bool Prepare()
-    {
-        return !OperatingSystem.IsAndroid();
-    }
+    private const string ArchitectEntry = "THE_ARCHITECT";
 
-    [HarmonyPostfix]
-    [HarmonyPatch(typeof(TheArchitect), nameof(TheArchitect.DefineDialogues))]
-    static void AddPigDialogue(ref AncientDialogueSet __result)
+    [HarmonyPrefix]
+    [HarmonyPatch(typeof(AncientDialogueSet), nameof(AncientDialogueSet.PopulateLocKeys))]
+    static void Prefix(AncientDialogueSet __instance, string ancientEntry)
     {
-        var pigId = "YUWANCARD-PIG";
-        if (__result.CharacterDialogues.ContainsKey(pigId))
+        if (ancientEntry != ArchitectEntry)
             return;
 
-        var dialogues = AncientDialogueUtil.GetDialoguesForKey("ancients", "THE_ARCHITECT.talk.YUWANCARD-PIG.");
-        if (dialogues.Count > 0)
-            __result.CharacterDialogues[pigId] = dialogues;
+        foreach (var character in ModelDb.AllCharacters)
+        {
+            if (character is not IYuWanCharacter)
+                continue;
+
+            var characterId = character.Id.Entry;
+            if (__instance.CharacterDialogues.ContainsKey(characterId))
+                continue;
+
+            var dialogues = AncientDialogueUtil.GetDialoguesForKey("ancients", $"{ArchitectEntry}.talk.{characterId}.");
+            if (dialogues.Count > 0)
+                __instance.CharacterDialogues[characterId] = dialogues;
+        }
     }
 }
