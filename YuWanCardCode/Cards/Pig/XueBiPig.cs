e5462934c33a8f96f1f767a5fcd825d3b940fcf8
YuWan_SAMA <145727116+YuWan886@users.noreply.github.com>
2026-06-13 14:19:14 +0800

    feat: 新增雪碧猪和巧乐兹猪食物猪卡牌
      - 新增 XueBiPig 雪碧猪卡牌，1费技能牌，获得死亡之舞和残影
      - 新增 QiaoLeZiPig 巧乐兹猪卡牌，1费技能牌，获得敏捷，消耗

---
+++ YuWanCardCode/Cards/Pig/XueBiPig.cs
@@ -0,0 +1,38 @@
+using YuWanCard.Core.Abstracts;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Models.Powers;
+using YuWanCard.Characters;
+using YuWanCard.Utils;
+
+namespace YuWanCard.Cards;
+
+[Pool(typeof(PigCardPool))]
+public class XueBiPig : YuWanCardModel
+{
+    public XueBiPig() : base(
+        baseCost: 1,
+        type: CardType.Skill,
+        rarity: CardRarity.Token,
+        target: TargetType.Self)
+    {
+        WithPower<DanseMacabrePower>(2);
+        WithPower<BlurPower>(1);
+        WithKeywords(CardKeyword.Exhaust);
+        WithTags(YuWanTags.FoodPig);
+    }
+
+    protected override void OnUpgrade()
+    {
+        EnergyCost.UpgradeBy(-1);
+    }
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        await CardUtils.RecordFoodPigPlayed(this);
+        await CreatureCmd.TriggerAnim(Owner.Creature, "Cast", Owner.Character.CastAnimDelay);
+        await PowerCmd.Apply<AfterimagePower>(Owner.Creature, DynamicVars["AfterimagePower"].BaseValue, Owner.Creature, this);
+        await PowerCmd.Apply<BlurPower>(Owner.Creature, DynamicVars["BlurPower"].BaseValue, Owner.Creature, this);
+    }
+}
