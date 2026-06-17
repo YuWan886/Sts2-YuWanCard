5a27be0a623e59ce25120c2c604e5d8bcd49a1a8
YuWan_SAMA <145727116+YuWan886@users.noreply.github.com>
2026-06-17 23:16:10 +0800

    feat: 新增"假如只有"系列遗物与"猪忙的过来"卡牌
      - 新增"假如只有古钱币""假如只有白星""假如只有火龙果"
      - 新增"猪忙的过来"牌，打出手牌中所有牌并获得4层懒惰

---
+++ YuWanCardCode/Cards/Colorless/PigBusyCome.cs
@@ -0,0 +1,44 @@
+using YuWanCard.Core.Abstracts;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Combat;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Models.CardPools;
+using MegaCrit.Sts2.Core.Models.Powers;
+
+namespace YuWanCard.Cards;
+
+[Pool(typeof(ColorlessCardPool))]
+public class PigBusyCome : YuWanCardModel
+{
+    public PigBusyCome() : base(
+        baseCost: 3,
+        type: CardType.Skill,
+        rarity: CardRarity.Rare,
+        target: TargetType.Self)
+    {
+        WithPower<SlothPower>(4);
+        WithKeyword(CardKeyword.Exhaust, UpgradeType.Remove);
+    }
+
+    protected override void OnUpgrade()
+    {
+    }
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        var cardsToPlay = PileType.Hand.GetPile(Owner).Cards
+            .Where(card => card != this)
+            .ToList();
+
+        foreach (var card in cardsToPlay)
+        {
+            if (CombatManager.Instance.IsOverOrEnding)
+                break;
+
+            await CardCmd.AutoPlay(choiceContext, card, null);
+        }
+
+        await PowerCmd.Apply<SlothPower>(Owner.Creature, DynamicVars["SlothPower"].BaseValue, Owner.Creature, this);
+    }
+}
---
+++ YuWanCardCode/Relics/WhatIf/WhatIfDragonFruit.cs
@@ -0,0 +1,42 @@
+using MegaCrit.Sts2.Core.Entities.Players;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Models.Relics;
+using MegaCrit.Sts2.Core.Rewards;
+using MegaCrit.Sts2.Core.Rooms;
+using MegaCrit.Sts2.Core.Runs;
+using YuWanCard.RelicPools;
+
+namespace YuWanCard.Relics;
+
+[Pool(typeof(WhatIfRelicPool))]
+public class WhatIfDragonFruit : WhatIfRelicModel, IWhatIfUniformRelicSource
+{
+    public WhatIfDragonFruit() : base(true)
+    {
+    }
+
+    public override bool TryModifyRewards(Player player, List<Reward> rewards, AbstractRoom? room)
+    {
+        if (player != Owner || Owner == null)
+        {
+            return false;
+        }
+
+        var fruitModel = ModelDb.Relic<DragonFruit>();
+
+        for (int i = 0; i < rewards.Count; i++)
+        {
+            if (rewards[i] is RelicReward)
+            {
+                rewards[i] = new RelicReward(fruitModel.ToMutable(), player);
+            }
+        }
+
+        return true;
+    }
+
+    public RelicModel GetUniformRelic(IRunState runState)
+    {
+        return ModelDb.Relic<DragonFruit>();
+    }
+}
